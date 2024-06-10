ff718173a44f4449c12e35e7dd096075157d27da
mollyas <166743060+mollyas@users.noreply.github.com>
2024-06-10 14:17:17 -0500

    Change numbers

--- add_numbers.py
+++ add_numbers.py
@@ -6,7 +6,7 @@ def mul(x, y):
 
 def main():
     print (f"The sum of 5 and 8 is {add(5,8)}.")
-    print (f"The product of 5 and 8 is {mul(5, 8)}")
+    print (f"The product of 6 and 9 is {mul(6, 9)}")
 
 if __name__ == "__main__":
     main()
