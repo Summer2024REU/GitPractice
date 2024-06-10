81dce1a9e4092791c64df901b53f7ea1bb80a5be
mollyas <166743060+mollyas@users.noreply.github.com>
2024-06-10 14:15:18 -0500

    Added multiplication function

--- add_numbers.py
+++ add_numbers.py
@@ -1,8 +1,12 @@
 def add(x, y):
     return x + y
 
+def mul(x, y):
+    return x*y
+
 def main():
     print (f"The sum of 5 and 8 is {add(5,8)}.")
+    print (f"The product of 5 and 8 is {mul(5, 8)}")
 
 if __name__ == "__main__":
     main()
